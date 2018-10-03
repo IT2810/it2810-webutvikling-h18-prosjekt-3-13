b17dcb89a36f4a81271173b7f1674e19b700ec1b
Tobias Skjelvik <trampcha@gmail.com>
2018-10-03 14:52:00 +0200

    la til styles filer og funksjonalitet for themebytte #2

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Component } from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import styles from './styles.js';
 
 export default class SettingsScreen extends Component{
+
 
   static navigationOptions = {
     title: 'Settings'
   };
 
+  handleThemeOnePress = () => {
+    const callBackFunction = this.props.screenProps.handleThemeChange;
+    callBackFunction("nightmode");
+  }
+
+  handleThemeTwoPress = () => {
+    const callBackFunction = this.props.screenProps.handleThemeChange;
+    callBackFunction("white");
+  }
+
   render(){
+    const {screenProps} = this.props;
     return(
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Settings Screen</Text>
+      <View style={[styles.container, {backgroundColor: screenProps.bgColor}]}>
+        <Text style={{color: screenProps.color}}>Settings Screen</Text>
+        <Text style={{color: screenProps.color}}>Press a button to change the colortheme</Text>
+        <Button title="Nightmode" onPress={this.handleThemeOnePress} />
+        <Button title="Defaultmode" onPress={this.handleThemeTwoPress} />
       </View>
     );
   }
